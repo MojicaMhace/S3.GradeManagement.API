08fe84d64312bcac89f60f57e825305cf023dc80
MojicaMhace <168732203+MojicaMhace@users.noreply.github.com>
2024-11-16 21:36:18 +0800

    Delete GradeManagementAPI directory

--- GradeManagementAPI/Credential.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GradeManagementAPI
-{
-    public class Credential
-    {
-        public string StudentName { get; set; }
-        public string CourseSection { get; set; }
-        public double Average { get; set; }
-    }
-}
